30e00812b1314a62a808b5be77c0f37478656856
DugganDuggan <aidan.duggan@live.co.uk>
2024-11-19 17:52:28 +0000

    View Media Details

---
+++ src/app/media-details/media-details.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-media-details',
+  standalone: true,
+  imports: [],
+  templateUrl: './media-details.component.html',
+  styleUrl: './media-details.component.css'
+})
+export class MediaDetailsComponent {
+
+}
