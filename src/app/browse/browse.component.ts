948a6708108047b14312d724477bcf2fd88f56ed
Aidan Duggan <aidan.duggan@live.co.uk>
2024-11-19 17:33:44 +0000

    Browse Media Page

--- src/app/browse/browse.component.ts
+++ src/app/browse/browse.component.ts
@@ -1,13 +1,22 @@
-import { Component } from '@angular/core';
-import { RouterEvent, RouterLink } from '@angular/router';
+import { NgFor } from '@angular/common';
+import { Component, signal } from '@angular/core';
 
 @Component({
   selector: 'app-browse',
   standalone: true,
-  imports: [RouterLink],
+  imports: [NgFor],
   templateUrl: './browse.component.html',
   styleUrl: './browse.component.css'
 })
 export class BrowseComponent {
+  genres = signal(['Fiction', 'Non-Fiction', 'Sci-Fi', 'Fantasy', 'Dystopian', 'Action & Adventure', 'Mystery', 'Horror', 'Thriller ', 'Romance', 'Graphic Novel', 'Short Story', 'Childrens', 'History', 'True Crime', 'Educational', 'Science & Technology']);
+  types = signal(['Book', 'Journal', 'Periodical', 'CD', 'DVD', 'Game'])
+
+  selectedOption = signal<string | null>(null);
+
+  // Function to handle selection
+  onFilterChange(selected: string): void {
+    this.selectedOption.set(selected);
+  }
 
 }
