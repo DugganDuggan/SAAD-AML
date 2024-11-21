dcf33a2fd805f66a56ebb25e6061e6f915519030
DugganDuggan <aidan.duggan@live.co.uk>
2024-11-21 14:59:05 +0000

    Borrow/Reserve Buttons

--- src/app/media-details/media-details.component.ts
+++ src/app/media-details/media-details.component.ts
@@ -1,12 +1,27 @@
-import { Component } from '@angular/core';
+import { NgIf } from '@angular/common';
+import { Component, signal } from '@angular/core';
 
 @Component({
   selector: 'app-media-details',
   standalone: true,
-  imports: [],
+  imports: [NgIf],
   templateUrl: './media-details.component.html',
   styleUrl: './media-details.component.css'
 })
 export class MediaDetailsComponent {
+
+  borrowHidden = signal(false); // Initial state is hidden
+  reserveHidden = signal(false); // Initial state is hidden
+
+  showBorrowPopup(){
+    this.borrowHidden.set(!this.borrowHidden());
+    console.log(this.borrowHidden())
+  }
+
+  showReservePopup() {
+    this.reserveHidden.set(!this.reserveHidden());
+    console.log(this.reserveHidden())
+
+  }
 
 }
