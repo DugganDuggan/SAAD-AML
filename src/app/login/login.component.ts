ea6428eb2c5e1cb214fc9aaca79c0d4831e0f15c
DugganDuggan <aidan.duggan@live.co.uk>
2024-11-28 15:09:25 +0000

    Fixed

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,8 +1,11 @@
 import { LoginService } from '../services/login.service';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
+  standalone: true,
+  imports: [FormsModule],
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
