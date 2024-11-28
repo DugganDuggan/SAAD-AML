7d030b1f210922441b6d852fbe3646c1afc8fdcd
SubhanAmjad2002 <subhan1amjad@icloud.com>
2024-11-28 15:01:52 +0000

    Registration log in page

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,18 +1,17 @@
-import { Component, inject } from '@angular/core';
 import { LoginService } from '../services/login.service';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-login',
-  standalone: true,
-  imports: [],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css',
-
+  styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+  username: string = '';
+  password: string = '';
 
-  logInService = inject(LoginService);
-  updateLogIn() {
-    this.logInService.loggedInTrue();
+  onSubmit() {
+    console.log('Login submitted:', { username: this.username, password: this.password });
+    // Add your login logic here
   }
 }
