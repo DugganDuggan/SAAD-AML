c8acfe197b4b1c0723290b9c3f0e219b0dd310f5
DugganDuggan <aidan.duggan@live.co.uk>
2024-11-28 15:18:48 +0000

    Manage Inventory Page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,4 +1,5 @@
 import { Routes } from '@angular/router';
+import { ManageInventoryComponent } from './manage-inventory/manage-inventory.component';
 
 export const routes: Routes = [
     {path: '', pathMatch: 'full', loadComponent: () => {return import('./home/home.component').then((m) => m.HomeComponent)}},
@@ -6,6 +7,7 @@ export const routes: Routes = [
     {path: 'login', loadComponent: () => {return import('./login/login.component').then((m) => m.LoginComponent)}},
     {path: 'account', loadComponent: () => {return import('./account/account.component').then((m) => m.AccountComponent)}},
     {path: 'browse', loadComponent: () => {return import('./browse/browse.component').then((m) => m.BrowseComponent)}},
-    {path: 'media-details', loadComponent: () => {return import('./media-details/media-details.component').then((m) => m.MediaDetailsComponent)}}
+    {path: 'media-details', loadComponent: () => {return import('./media-details/media-details.component').then((m) => m.MediaDetailsComponent)}},
+    {path: 'manage-inventory', loadComponent: () => {return import('./manage-inventory/manage-inventory.component').then((m) => m.ManageInventoryComponent)}}
 
 ];
