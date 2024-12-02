b3ce87db98f1078429028af1fd83900a2ca3ba24
SubhanAmjad2002 <subhan1amjad@icloud.com>
2024-12-02 15:21:48 +0000

    Wireframe update

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,11 +1,8 @@
 import { LoginService } from '../services/login.service';
 import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
-  standalone: true,
-  imports: [FormsModule],
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
