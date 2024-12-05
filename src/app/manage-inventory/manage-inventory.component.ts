5ab73809db5db43276894191d3407ba75ed6e89e
SubhanAmjad2002 <subhan1amjad@icloud.com>
2024-12-05 14:57:06 +0000

    Manage Inventory Page complete

--- src/app/manage-inventory/manage-inventory.component.ts
+++ src/app/manage-inventory/manage-inventory.component.ts
@@ -3,10 +3,20 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-manage-inventory',
   standalone: true,
-  imports: [],
   templateUrl: './manage-inventory.component.html',
-  styleUrl: './manage-inventory.component.css'
+  styleUrls: ['./manage-inventory.component.css']
 })
 export class ManageInventoryComponent {
+  // Example data
+  newOrders = 57;
+  previousOrders = 26;
+  overdueReturns = 5;
+  lowStock = 3;
 
+  ordersData = [
+    { month: 'Jan', newOrders: 10, overdueReturns: 1 },
+    { month: 'Feb', newOrders: 15, overdueReturns: 3 },
+    { month: 'Mar', newOrders: 12, overdueReturns: 2 },
+    // More data here...
+  ];
 }
