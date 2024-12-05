e0e429ec9f816701aed75398ead2c37cba15878a
DugganDuggan <aidan.duggan@live.co.uk>
2024-12-05 11:45:14 +0000

    Fix manage inventory page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,10 @@ import { Routes } from '@angular/router';
 import { ManageInventoryComponent } from './manage-inventory/manage-inventory.component';
 
 export const routes: Routes = [
-    {path: '', pathMatch: 'full', loadComponent: () => {return import('./home/home.component').then((m) => m.HomeComponent)}},
     {path: 'register', loadComponent: () => {return import('./register/register.component').then((m) => m.RegisterComponent)}},
     {path: 'login', loadComponent: () => {return import('./login/login.component').then((m) => m.LoginComponent)}},
     {path: 'account', loadComponent: () => {return import('./account/account.component').then((m) => m.AccountComponent)}},
     {path: 'browse', loadComponent: () => {return import('./browse/browse.component').then((m) => m.BrowseComponent)}},
+    {path: 'manage-inventory', loadComponent: () => {return import('./manage-inventory/manage-inventory.component').then((m) => m.ManageInventoryComponent)}},
     {path: 'media-details/:id', loadComponent: () => {return import('./media-details/media-details.component').then((m) => m.MediaDetailsComponent)}}
 ];
